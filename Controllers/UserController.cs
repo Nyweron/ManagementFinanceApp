a3c5ff44344d641c708ddc55fb59f14d92487627
Przemysław Juśkiewicz <nyweron@gmail.com>
2019-01-09 19:32:52 +0100

    Create action in user CRUD

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -1,8 +1,11 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AutoMapper;
 using ManagementFinanceApp.Data;
 using ManagementFinanceApp.Entities;
+using ManagementFinanceApp.Models;
 using ManagementFinanceApp.Repository.User;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -13,13 +16,15 @@ namespace ManagementFinanceApp.Controllers
   [ApiController]
   public class UserController : Controller
   {
-    private readonly ManagementFinanceAppDbContext _context;
-    private IUserRepository _userRepository;
 
-    public UserController(ManagementFinanceAppDbContext context, IUserRepository userRepository)
+    private IUserRepository _userRepository;
+    private IMapper _mapper;
+    public UserController(IUserRepository userRepository,
+      IMapper mapper
+    )
     {
-      _context = context;
       _userRepository = userRepository;
+      _mapper = mapper;
     }
 
     [HttpGet]
@@ -29,17 +34,108 @@ namespace ManagementFinanceApp.Controllers
       return Ok(userEntities);
     }
 
-    [HttpGet("{id}")]
-    public async Task<ActionResult<User>> GetUser(int id)
+    [HttpGet("{userId}")]
+    public IActionResult Get(int userId)
     {
-      var todoItem = await _context.Users.FindAsync(id);
+      try
+      {
+        if (!_userRepository.UserExistsAsync(userId).Result)
+        {
+          // _logger.LogInformation($"User with id {userId} wasn't found when accessing to UserController/Get(int userId).");
+          return NotFound();
+        }
 
-      if (todoItem == null)
+        var userEntities = _userRepository.GetAsync(userId).Result;
+        return Ok(userEntities);
+      }
+      catch (Exception ex)
+      {
+        // _logger.LogCritical($"Exception {userId}.", ex);
+        return StatusCode(500, "Problem with your request.");
+      }
+    }
+
+    [HttpPost]
+    public IActionResult Post([FromBody] UserDto user)
+    {
+      if (user == null)
+      {
+        //_logger.LogInformation($"User is empty when accessing to UserController/Post(UserDto user).");
+        return BadRequest();
+      }
+
+      if (_userRepository.EmailExistsAsync(user.Email).Result)
+      {
+        // _logger.LogInformation($"The Email {user.Email} exist in database, use other email. UserController/Post(UserDto user).");
+        return BadRequest($"The Email {user.Email} exist, user other email.");
+      }
+
+      if (!ModelState.IsValid)
+      {
+        return BadRequest(ModelState);
+      }
+
+      var userEntity = _mapper.Map<User>(user);
+      _userRepository.AddAsync(userEntity);
+
+      if (!_userRepository.SaveAsync().Result)
+      {
+        // _logger.LogError($"Add User is not valid. Error in SaveAsync(). When accessing to UserController/Post");
+        return StatusCode(500, "A problem happend while handling your request.");
+      }
+
+      //TODO: Implement Realistic Implementation
+      return Created("", null);
+    }
+
+    [HttpPut("{userId}")]
+    public IActionResult Put([FromBody] UserDto user, int userId)
+    {
+      if (user == null)
+      {
+        return BadRequest();
+      }
+
+      if (!_userRepository.UserExists(userId))
       {
         return NotFound();
       }
 
-      return todoItem;
+      if (!ModelState.IsValid)
+      {
+        return BadRequest(ModelState);
+      }
+
+      var updatedUser = _mapper.Map<User>(user);
+      _userRepository.UpdateUser(updatedUser);
+
+      if (!_userRepository.Save())
+      {
+        return StatusCode(500, "A problem happend while handling your request.");
+      }
+      //TODO: Implement Realistic Implementation
+      return Ok();
+    }
+
+    [HttpDelete("{id}")]
+    public IActionResult Delete(int id)
+    {
+      if (!_userRepository.UserExistsAsync(id).Result)
+      {
+        // _logger.LogInformation($"User with id: {id} is not exist. When accessing to UserController/Delete(int id).");
+        return NotFound();
+      }
+
+      var user = _userRepository.GetAsync(id).Result;
+      _userRepository.Remove(user);
+
+      if (!_userRepository.SaveAsync().Result)
+      {
+        //_logger.LogError($"Delete User is not valid. Error in SaveAsync(). When accessing to UserController/Delete");
+        return StatusCode(500, "A problem happend while handling your request.");
+      }
+      //TODO: Implement Realistic Implementation
+      return Ok();
     }
 
   }
