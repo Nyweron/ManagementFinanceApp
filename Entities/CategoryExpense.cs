0e922a064796336288fdbe12e5f61d404600a3bc
Przemysław Juśkiewicz <nyweron@gmail.com>
2019-02-11 13:38:04 +0100

    Add entity CategoryExpsense (pl. KategorieWydatkow)

--- Entities/CategoryExpense.cs
+++ Entities/CategoryExpense.cs
@@ -1,7 +1,28 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace ManagementFinanceApp.Entities
 {
-    public class CategoryExpense
-    {
-        
-    }
+  /* KategorieWydatkow - CategoryExpense */
+  public class CategoryExpense
+  {
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public int Id { get; set; }
+
+    [Required]
+    public string Description { get; set; }
+
+    [Required]
+    public bool IsDeleted { get; set; }
+
+    [Required]
+    public int Weight { get; set; }
+
+    [Required]
+    public CategoryGroup CategoryGroup { get; set; }
+
+    [Required]
+    public int CategoryGroupId { get; set; }
+  }
 }
