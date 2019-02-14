d84b11e1f179f0ac0247d5b4dfce3f649ff48f9e
Przemysław Juśkiewicz <nyweron@gmail.com>
2019-02-14 20:41:43 +0100

    Add entity and migration CategoryExpenses

--- Data/ManagementFinanceAppDbContext.cs
+++ Data/ManagementFinanceAppDbContext.cs
@@ -13,7 +13,7 @@ namespace ManagementFinanceApp.Data
     public DbSet<CategoryIncome> CategoryIncomes { get; set; }
     public DbSet<Income> Incomes { get; set; }
     public DbSet<TransferHistory> TransferHistories { get; set; }
-    // public DbSet<CategoryExpense> CategoryExpenses { get; set; }
+    public DbSet<CategoryExpense> CategoryExpenses { get; set; }
     // public DbSet<Expense> Expenses { get; set; }
     // public DbSet<Restriction> Restrictions { get; set; }
     // public DbSet<Frequency> Frequencies { get; set; }
---
+++ Migrations/20190214194058_CategoryExpense.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ManagementFinanceApp.Migrations
+{
+    public partial class CategoryExpense : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_CategoryExpense_CategoryGroups_CategoryGroupId",
+                table: "CategoryExpense");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_CategoryExpense",
+                table: "CategoryExpense");
+
+            migrationBuilder.RenameTable(
+                name: "CategoryExpense",
+                newName: "CategoryExpenses");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_CategoryExpense_CategoryGroupId",
+                table: "CategoryExpenses",
+                newName: "IX_CategoryExpenses_CategoryGroupId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_CategoryExpenses",
+                table: "CategoryExpenses",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_CategoryExpenses_CategoryGroups_CategoryGroupId",
+                table: "CategoryExpenses",
+                column: "CategoryGroupId",
+                principalTable: "CategoryGroups",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_CategoryExpenses_CategoryGroups_CategoryGroupId",
+                table: "CategoryExpenses");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_CategoryExpenses",
+                table: "CategoryExpenses");
+
+            migrationBuilder.RenameTable(
+                name: "CategoryExpenses",
+                newName: "CategoryExpense");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_CategoryExpenses_CategoryGroupId",
+                table: "CategoryExpense",
+                newName: "IX_CategoryExpense_CategoryGroupId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_CategoryExpense",
+                table: "CategoryExpense",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_CategoryExpense_CategoryGroups_CategoryGroupId",
+                table: "CategoryExpense",
+                column: "CategoryGroupId",
+                principalTable: "CategoryGroups",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
