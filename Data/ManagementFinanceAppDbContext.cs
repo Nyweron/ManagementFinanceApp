67e8395c8b1d0bfd2dba00aff1d9047b16214dd9
Przemysław Juśkiewicz <nyweron@gmail.com>
2019-02-14 20:34:59 +0100

    Add entity and migration User

--- Data/ManagementFinanceAppDbContext.cs
+++ Data/ManagementFinanceAppDbContext.cs
@@ -8,21 +8,21 @@ namespace ManagementFinanceApp.Data
     public ManagementFinanceAppDbContext(DbContextOptions<ManagementFinanceAppDbContext> options) : base(options) { }
 
     public DbSet<User> Users { get; set; }
-    public DbSet<CategoryGroup> CategoryGroups { get; set; }
-    public DbSet<CategorySaving> CategorySavings { get; set; }
-    public DbSet<CategoryIncome> CategoryIncomes { get; set; }
-    public DbSet<Income> Incomes { get; set; }
-    public DbSet<TransferHistory> TransferHistories { get; set; }
-    public DbSet<CategoryExpense> CategoryExpenses { get; set; }
-    public DbSet<Expense> Expenses { get; set; }
-    public DbSet<Restriction> Restrictions { get; set; }
-    public DbSet<Frequency> Frequencies { get; set; }
-    public DbSet<Saving> Savings { get; set; }
-    public DbSet<StandingOrder> StandingOrders { get; set; }
-    public DbSet<StandingOrderHistory> StandingOrderHistories { get; set; }
-    public DbSet<SavingState> SavingStates { get; set; }
-    public DbSet<Plan> Plans { get; set; }
-    public DbSet<Investment> Investments { get; set; }
-    public DbSet<InvestmentSchedule> InvestmentSchedules { get; set; }
+    // public DbSet<CategoryGroup> CategoryGroups { get; set; }
+    // public DbSet<CategorySaving> CategorySavings { get; set; }
+    // public DbSet<CategoryIncome> CategoryIncomes { get; set; }
+    // public DbSet<Income> Incomes { get; set; }
+    // public DbSet<TransferHistory> TransferHistories { get; set; }
+    // public DbSet<CategoryExpense> CategoryExpenses { get; set; }
+    // public DbSet<Expense> Expenses { get; set; }
+    // public DbSet<Restriction> Restrictions { get; set; }
+    // public DbSet<Frequency> Frequencies { get; set; }
+    // public DbSet<Saving> Savings { get; set; }
+    // public DbSet<StandingOrder> StandingOrders { get; set; }
+    // public DbSet<StandingOrderHistory> StandingOrderHistories { get; set; }
+    // public DbSet<SavingState> SavingStates { get; set; }
+    // public DbSet<Plan> Plans { get; set; }
+    // public DbSet<Investment> Investments { get; set; }
+    // public DbSet<InvestmentSchedule> InvestmentSchedules { get; set; }
   }
 }
