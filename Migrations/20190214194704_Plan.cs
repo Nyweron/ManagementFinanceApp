7c33af80b588a2967b243600ff9635a3e46722c1
Przemysław Juśkiewicz <nyweron@gmail.com>
2019-02-14 20:47:08 +0100

    Add entity and migration Plan

---
+++ Migrations/20190214194704_Plan.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace ManagementFinanceApp.Migrations
+{
+    public partial class Plan : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Plans",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.SerialColumn),
+                    Name = table.Column<string>(nullable: false),
+                    Amount = table.Column<double>(nullable: false),
+                    CategoryId = table.Column<int>(nullable: false),
+                    IsDone = table.Column<bool>(nullable: false),
+                    IsAddedToQueue = table.Column<bool>(nullable: false),
+                    PlanType = table.Column<int>(nullable: false),
+                    Date = table.Column<DateTime>(nullable: false),
+                    Comment = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Plans", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Plans");
+        }
+    }
+}
