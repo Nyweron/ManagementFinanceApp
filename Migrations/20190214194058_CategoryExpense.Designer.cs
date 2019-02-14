1a69691aef1c36cd9ab37c3aa3ff845bd7c217b0
Przemysław Juśkiewicz <nyweron@gmail.com>
2019-02-14 20:41:43 +0100

    Add entity and migration CategoryExpenses

--- Migrations/20190214194058_CategoryExpense.Designer.cs
+++ Migrations/20190214194058_CategoryExpense.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using ManagementFinanceApp.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
 namespace ManagementFinanceApp.Migrations
 {
     [DbContext(typeof(ManagementFinanceAppDbContext))]
-    partial class ManagementFinanceAppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20190214194058_CategoryExpense")]
+    partial class CategoryExpense
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
