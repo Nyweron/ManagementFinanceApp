351cdcddea95f159631199046d4ea2b4635d012f
Przemysław Juśkiewicz <nyweron@gmail.com>
2019-02-14 20:44:49 +0100

    Add entity and migration Saving

---
+++ Migrations/20190214194404_Saving.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace ManagementFinanceApp.Migrations
+{
+    public partial class Saving : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Savings",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.SerialColumn),
+                    HowMuch = table.Column<double>(nullable: false),
+                    Date = table.Column<DateTime>(nullable: false),
+                    Comment = table.Column<string>(nullable: true),
+                    SavingType = table.Column<int>(nullable: false),
+                    CategorySavingId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Savings", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Savings_CategorySavings_CategorySavingId",
+                        column: x => x.CategorySavingId,
+                        principalTable: "CategorySavings",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Savings_CategorySavingId",
+                table: "Savings",
+                column: "CategorySavingId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Savings");
+        }
+    }
+}
