06c1e553dcdd09877774395721befd2700e7a696
Przemysław Juśkiewicz <nyweron@gmail.com>
2019-02-14 20:43:44 +0100

    Add entity and migration Frequency

--- Data/ManagementFinanceAppDbContext.cs
+++ Data/ManagementFinanceAppDbContext.cs
@@ -16,7 +16,7 @@ namespace ManagementFinanceApp.Data
     public DbSet<CategoryExpense> CategoryExpenses { get; set; }
     public DbSet<Expense> Expenses { get; set; }
     public DbSet<Restriction> Restrictions { get; set; }
-    // public DbSet<Frequency> Frequencies { get; set; }
+    public DbSet<Frequency> Frequencies { get; set; }
     // public DbSet<Saving> Savings { get; set; }
     // public DbSet<StandingOrder> StandingOrders { get; set; }
     // public DbSet<StandingOrderHistory> StandingOrderHistories { get; set; }
---
+++ Migrations/20190214194340_Frequency.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace ManagementFinanceApp.Migrations
+{
+    public partial class Frequency : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Frequencies",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.SerialColumn),
+                    Description = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Frequencies", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Frequencies");
+        }
+    }
+}
