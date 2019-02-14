f1ff82662221e60680c6ccf17a11cd6755765be8
Przemysław Juśkiewicz <nyweron@gmail.com>
2019-02-14 20:38:11 +0100

    Add entity and migration CategoryIncome

---
+++ Migrations/20190214193758_CategoryIncomes.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ManagementFinanceApp.Migrations
+{
+    public partial class CategoryIncomes : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_CategoryIncome_CategoryGroups_CategoryGroupId",
+                table: "CategoryIncome");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_CategoryIncome",
+                table: "CategoryIncome");
+
+            migrationBuilder.RenameTable(
+                name: "CategoryIncome",
+                newName: "CategoryIncomes");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_CategoryIncome_CategoryGroupId",
+                table: "CategoryIncomes",
+                newName: "IX_CategoryIncomes_CategoryGroupId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_CategoryIncomes",
+                table: "CategoryIncomes",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_CategoryIncomes_CategoryGroups_CategoryGroupId",
+                table: "CategoryIncomes",
+                column: "CategoryGroupId",
+                principalTable: "CategoryGroups",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_CategoryIncomes_CategoryGroups_CategoryGroupId",
+                table: "CategoryIncomes");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_CategoryIncomes",
+                table: "CategoryIncomes");
+
+            migrationBuilder.RenameTable(
+                name: "CategoryIncomes",
+                newName: "CategoryIncome");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_CategoryIncomes_CategoryGroupId",
+                table: "CategoryIncome",
+                newName: "IX_CategoryIncome_CategoryGroupId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_CategoryIncome",
+                table: "CategoryIncome",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_CategoryIncome_CategoryGroups_CategoryGroupId",
+                table: "CategoryIncome",
+                column: "CategoryGroupId",
+                principalTable: "CategoryGroups",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
