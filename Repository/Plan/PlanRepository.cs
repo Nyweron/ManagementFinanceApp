2dad60eb162542e1b20d905ab506b4cea08730ee
Przemysław Juśkiewicz <nyweron@gmail.com>
2019-02-17 20:05:49 +0100

    Add SaveAsync method to PlanRepository

--- Repository/Plan/PlanRepository.cs
+++ Repository/Plan/PlanRepository.cs
@@ -1,12 +1,20 @@
+using System.Threading.Tasks;
 using ManagementFinanceApp.Data;
 
 namespace ManagementFinanceApp.Repository.Plan
 {
   public class PlanRepository : Repository<Entities.Plan>, IPlanRepository
   {
-    public PlanRepository(ManagementFinanceAppDbContext context) : base(context)
-    {
+    public PlanRepository(ManagementFinanceAppDbContext context) : base(context) { }
 
+    public ManagementFinanceAppDbContext ManagementFinanceAppDbContext
+    {
+      get { return Context as ManagementFinanceAppDbContext; }
+    }
+    public async Task<bool> SaveAsync()
+    {
+      var result = await ManagementFinanceAppDbContext.SaveChangesAsync();
+      return (result >= 0);
     }
   }
 }
