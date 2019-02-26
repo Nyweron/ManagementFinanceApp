bd48d728cb848d38c24082dbc9a525556203b368
Przemysław Juśkiewicz <nyweron@gmail.com>
2019-02-26 15:43:24 +0100

    Add Frequencies sql insert

---
+++ SQL-Scripts/Frequencies.sql
@@ -0,0 +1,10 @@
+--Frequencies
+INSERT INTO public."Frequencies"("Id","Description") VALUES (-1,'Nigdy');
+INSERT INTO public."Frequencies"("Id","Description") VALUES (0,'Codziennie');
+INSERT INTO public."Frequencies"("Id","Description") VALUES (1,'Co tydzień');
+INSERT INTO public."Frequencies"("Id","Description") VALUES (2,'Co miesiąc');
+INSERT INTO public."Frequencies"("Id","Description") VALUES (3,'Co dwa miesiące');
+INSERT INTO public."Frequencies"("Id","Description") VALUES (4,'Co kwartał');
+INSERT INTO public."Frequencies"("Id","Description") VALUES (5,'Co pół roku');
+INSERT INTO public."Frequencies"("Id","Description") VALUES (6,'Co rok');
+INSERT INTO public."Frequencies"("Id","Description") VALUES (7,'Uruchamiane ręcznie');
