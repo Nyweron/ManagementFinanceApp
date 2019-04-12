713f2432cfddf3c1ac49d5a49f885bf957f14a18
Przemysław Juśkiewicz <nyweron@gmail.com>
2019-04-12 20:31:34 +0200

    Add tests for CategoryExpenseController action delete

--- ManagementFinanceApp.Test/Controllers/CategoryExpenseControllerTest.cs
+++ ManagementFinanceApp.Test/Controllers/CategoryExpenseControllerTest.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
 using System.Web.Http;
 using AutoMapper;
@@ -55,7 +56,8 @@ namespace ManagementFinanceApp.Test.Controllers
       var categoryExpenseTest = GetTestCategoryExpenses().ToList() [secondItemFromList];
 
       var mock = new Mock<ICategoryExpenseRepository>();
-      mock.Setup(repo => repo.GetAsync(expectedIdOfCategoryExpense)).Returns(Task.FromResult(categoryExpenseTest));
+      mock.Setup(repo => repo.GetAsync(expectedIdOfCategoryExpense))
+        .Returns(Task.FromResult(categoryExpenseTest));
 
       var controller = new CategoryExpenseController(mock.Object, null);
 
@@ -75,29 +77,60 @@ namespace ManagementFinanceApp.Test.Controllers
       var expectedIdOfCategoryExpense = 2;
       var secondItemFromList = 1;
       var testModelCategoryExpense = new Entities.CategoryExpense { Id = 2, Description = "CategoryeExpense2", IsDeleted = false, Weight = 2, CategoryGroupId = 2 };
+
       var categoryExpenseTest = GetTestCategoryExpenses().ToList() [secondItemFromList];
 
-      var mock2 = new Mock<ICategoryExpenseRepository>();
-      mock2.Setup(repo => repo.GetAsync(expectedIdOfCategoryExpense)).Returns(Task.FromResult(categoryExpenseTest));
-
-      var controller2 = new CategoryExpenseController(mock2.Object, null);
-
       // act
-      var okObjectResult2 = await controller2.Get(expectedIdOfCategoryExpense) as OkObjectResult;
-      var result2 = okObjectResult2.Value as Entities.CategoryExpense;
 
       var mock = new Mock<ICategoryExpenseRepository>();
-
-      mock.Setup(repo => repo.RemoveAsync(testModelCategoryExpense)).Returns(Task.FromResult());
+      mock.Setup(repo => repo.GetAsync(expectedIdOfCategoryExpense)).Returns(Task.FromResult(testModelCategoryExpense));
+      mock.Setup(repo => repo.RemoveAsync(testModelCategoryExpense)).Returns(Task.FromResult(true));
 
       var controller = new CategoryExpenseController(mock.Object, null);
-
-      // act
-      var okObjectResult = await controller.Delete(expectedIdOfCategoryExpense) as OkObjectResult;
-      var result = okObjectResult.Value as Entities.CategoryExpense;
+      var noContentResult = await controller.Delete(expectedIdOfCategoryExpense) as NoContentResult;
 
       // assert
-      Assert.AreEqual(true, result);
+      Assert.NotNull(noContentResult, "noContentResult is null");
+      Assert.AreEqual(noContentResult.StatusCode, 204, "delete is not works");
+    }
+
+    [Test]
+    public async Task DeleteByIdCategoryExpenses_ShouldReturnNotFoundWhenGetAsync()
+    {
+      // arrange
+      var expectedIdOfCategoryExpense = 2;
+      var testModelCategoryExpense = new Entities.CategoryExpense { Id = 2, Description = "CategoryeExpense2", IsDeleted = false, Weight = 2, CategoryGroupId = 2 };
+
+      // act
+      var mock = new Mock<ICategoryExpenseRepository>();
+      mock.Setup(repo => repo.RemoveAsync(testModelCategoryExpense)).Returns(Task.FromResult(true));
+
+      var controller = new CategoryExpenseController(mock.Object, null);
+      var notFoundResult = await controller.Delete(expectedIdOfCategoryExpense) as NotFoundResult;
+
+      // assert
+      Assert.AreEqual(404, notFoundResult.StatusCode, "Not found result, not works");
+    }
+
+    [Test]
+    public async Task DeleteByIdCategoryExpenses_ShouldReturnInternalServerErrorCategoryExpense()
+    {
+      // arrange
+      var expectedIdOfCategoryExpense = 2;
+      var testModelCategoryExpense = new Entities.CategoryExpense { Id = 2, Description = "CategoryeExpense2", IsDeleted = false, Weight = 2, CategoryGroupId = 2 };
+
+      // act
+
+      var mock = new Mock<ICategoryExpenseRepository>();
+      mock.Setup(repo => repo.GetAsync(expectedIdOfCategoryExpense)).Returns(Task.FromResult(testModelCategoryExpense));
+      mock.Setup(repo => repo.RemoveAsync(testModelCategoryExpense)).Returns(Task.FromResult(false));
+
+      var controller = new CategoryExpenseController(mock.Object, null);
+      var noContentResult = await controller.Delete(expectedIdOfCategoryExpense) as ObjectResult;
+
+      // assert
+      Assert.NotNull(noContentResult, "noContentResult is null");
+      Assert.AreEqual(noContentResult.StatusCode, 500, "Internal server error");
     }
 
     private IEnumerable<Entities.CategoryExpense> GetTestCategoryExpenses()
