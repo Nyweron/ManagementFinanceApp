5859f6fcac24d4e37bc5bc970adbb008b1708ba5
Przemysław Juśkiewicz <nyweron@gmail.com>
2019-05-31 18:23:08 +0200

    Added new name of url

--- ClientApp/src/apiUrls.js
+++ ClientApp/src/apiUrls.js
@@ -1,2 +1,3 @@
 export const managementFinance = "http://localhost:54101/";
 export const personUrl = "http://localhost:54101/api/expense";
+export const apiUrlExpense = "http://localhost:54101/api/expense";
